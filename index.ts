7bf96185dcea5865eacae6ffa626e3d7be7bbdfe
Emmanuel Israel Jr <99355350+Emmanuel-Fofeyin@users.noreply.github.com>
2024-05-11 22:08:03 -0400

    done

--- index.ts
+++ index.ts
@@ -1,1 +1,31 @@
-console.log("Hello via Bun!");
+/*
+* This program calculates the factorial of a number.
+*
+* @author  Emmanuel FN
+* @version 1.0
+* @since   2024-05-11
+*/
+
+import { createPrompt } from 'bun-promptx'
+
+function factorial(someInt: number): number {
+  /*
+  * This function calculates the factorial using recursion.
+  */
+  if (someInt < 0) {
+    return -1
+  } else {
+    if (someInt <= 1) {
+      return 1
+    } else {
+      return (someInt * factorial(someInt - 1))
+    }
+  }
+}
+
+const userInput = createPrompt(`Entered = `)
+const someInt = parseInt(userInput.value)
+let factorialOfInt = factorial(someInt)
+console.log(`Returned = ${factorialOfInt}`)
+
+console.log(`\nDone.`)
